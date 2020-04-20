d9f4d87d4661b0aab42c0ba81d24ea7a8cb36e63
Siamak Mahmoudi <ssiasoft@gmail.com>
2020-04-20 20:00:14 +0430

    json system added & thumbnail added

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python -W ignore::DeprecationWarning
 
 from app import app
+from app import SpecieDb, db
 
 if __name__ == '__main__':
+    #
+    # for record in SpecieDb.SpecieDb.query.all():
+    #     record.update(record.code, record.code_name, "", record.schema)
+    #     db.session.commit()
+
     app.run(debug=True)
